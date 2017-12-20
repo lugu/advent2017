816c7d19abf8e6c7ad00c6a5d63fcbca467fbed8
Ludovic Guegan <lguegan@aldebaran-robotics.com>
2017-12-20 12:56:17 +0100

    day 18 part 2

--- src/main/scala/advent/day18/Main.scala
+++ src/main/scala/advent/day18/Main.scala
@@ -1,129 +1,131 @@
 package advent.day18
 
 import scala.io.Source
+import scala.collection.immutable.Queue
 import scala.util.parsing.combinator.RegexParsers
-import scala.util.parsing.combinator.token.Tokens
 
 object Main extends App {
 
   type Instruction = State => State
 
   object ReceiveInterrupt extends Exception
-  case class PendingInstruction(s: State) extends Exception
+  case class PendingInstruction(p: Prog) extends Exception
 
-  case class Bus(in: List[BigInt], out: List[BigInt]) {
-    def connect(o: Bus): Tuple2[Bus,Bus] = {
-      (Bus(in ++ o.out, List()), Bus(o.in ++ out, List()))
+  case class Pipe(in: Queue[BigInt] = Queue(), out: Queue[BigInt] = Queue()) {
+    def connect(o: Pipe): Tuple2[Pipe, Pipe] = {
+      (Pipe(in ++ o.out, Queue()), Pipe(o.in ++ out, Queue()))
     }
-    def send(v: BigInt) = Bus(in, out :+ v)
-    def receive: Tuple2[BigInt,Bus] = in match {
-      case List() => throw ReceiveInterrupt
-      case a :: rest => (a, Bus(rest, out))
-    }
+    def send(v: BigInt) = Pipe(in, out :+ v)
+    def receive: Tuple2[BigInt, Pipe] =
+      if (in.isEmpty) throw ReceiveInterrupt
+      else (in.head, Pipe(in.tail, out))
   }
 
-  @scala.annotation.tailrec
-  def runLoop(s: State, i: Seq[Instruction]): State = runLoop(s.step(i), i)
-
-  case class State(s: Map[Char, BigInt], pc: Int, bus: Bus) {
+  case class State(s: Map[Char, BigInt], pc: Int, pipe: Pipe) {
     def read(reg: Char): BigInt = s.getOrElse(reg, 0)
-    def write(reg: Char, v: BigInt) = State(s + (reg -> v), pc, bus)
+    def write(reg: Char, v: BigInt) = State(s + (reg -> v), pc, pipe)
     def p: Int = s('p').toInt
-    def next: State = State(s, pc + 1, bus)
-    def send(a: BigInt) = State(s, pc, bus.send(a))
-    def receive: Tuple2[BigInt,State] = {
-      val (a, b) = bus.receive
+    def incrPC: State = State(s, pc + 1, pipe)
+    def send(a: BigInt) = State(s, pc, pipe.send(a))
+    def receive: Tuple2[BigInt, State] = {
+      val (a, b) = pipe.receive
       (a, State(s, pc, b))
     }
-    def setPC(n: BigInt) = State(s, n.toInt, bus)
+    def setPC(n: BigInt) = State(s, n.toInt, pipe)
+    def step(i: Seq[Instruction]): State = i(pc)(this)
+    def setPipe(b: Pipe): State = State(s, pc, b)
 
-    def stepSafe(i: Seq[Instruction]): State = try {
+    def setInst(r: Char, v: BigInt): State = write(r, v)
+    def mulInst(r: Char, v: BigInt): State = write(r, v * read(r))
+    def jgzInst(t: BigInt, v: BigInt): State =
+      if (t <= 0) this else setPC(pc + v - 1)
+    def addInst(r: Char, v: BigInt): State = write(r, v + read(r))
+    def modInst(r: Char, v: BigInt): State = write(r, read(r) % v)
 
-      // println(input.toSeq(pc))
-      // println(this)
-      // val a = scala.io.StdIn.readLine()
-      // if (a != "") throw new Exception("Halted")
-
-      step(i)
-    } catch {
-      case PendingInstruction(state) => state
-    }
-    def step(i: Seq[Instruction]): State = try {
-      i(pc)(this).next
-    } catch {
-      case ReceiveInterrupt => throw new PendingInstruction(this)
-    }
-    def run(i: Seq[Instruction]): State = try {
-      runLoop(this, i)
-    } catch {
-      case PendingInstruction(state) => state
-    }
-    def isEqual(o: State): Boolean = (pc == o.pc && (s.toSet diff o.s.toSet).isEmpty)
-    def setBus(b: Bus): State = State(s, pc, b)
-    def updateBus(o: State): Tuple2[State,State] = {
-      val (nextBusA, nextBusB) = bus.connect(o.bus)
-      (setBus(nextBusA), o.setBus(nextBusB))
+    def sndInst(v: BigInt): State = send(v)
+    def rcvInst(r: Char): State = {
+      val (value, state) = receive
+      state.write(r, value)
     }
   }
-
 
   class InstructionParser extends RegexParsers {
     def value: Parser[Int] = "(-?\\d+)".r ^^ { case p => p.toInt }
-    def register: Parser[Char] = "(\\w)".r ^^ { case n => n.toCharArray.head }
-    def valueOrRegister: Parser[Either[Char,Int]] = (value | register) ^^ (b => b match {
-      case c: Char => Left(c)
-      case i: Int => Right(i)
-    })
+    def register: Parser[Char] = "([a-z])".r ^^ {
+      case n => n.toCharArray.head
+    }
+    def valueOrRegister: Parser[Either[Char, Int]] =
+      (value | register) ^^ (b =>
+        b match {
+          case c: Char => Left(c)
+          case i: Int => Right(i)
+        })
 
-    def twoOperandInst(op: String, f: (Char, BigInt) => Instruction) =
-      op ~> register ~ valueOrRegister ^^ {
-        case (a ~ b) => (s: State) => b match {
-          case Left(c) => f(a, s.read(c))(s)
-          case Right(i) => f(a, i)(s)
-        }
+    def makeInstructionParser(
+        name: String,
+        inst: (Char, BigInt) => Instruction): Parser[Instruction] =
+      name ~> register ~ valueOrRegister ^^ {
+        case (a ~ b) =>
+          b match {
+            case Left(c) =>
+              (s: State) =>
+                inst(a, s.read(c))(s)
+            case Right(i) =>
+              (s: State) =>
+                inst(a, i)(s)
+          }
       }
 
     def set: Parser[Instruction] =
-      twoOperandInst("set", {
-        case (r, v) =>
-          (s: State) =>
-            s.write(r, v)
-      })
+      makeInstructionParser(
+        "set",
+        (r: Char, v: BigInt) => (state: State) => state.setInst(r, v))
+
     def mul: Parser[Instruction] =
-      twoOperandInst("mul", {
-        case (r, v) =>
-          (s: State) =>
-            s.write(r, v * s.read(r))
-      })
-    def jmp: Parser[Instruction] =
-      twoOperandInst("jgz", {
-        case (r, v) =>
-          (s: State) =>
-            if (s.read(r) <= 0) s else s.setPC(s.pc + v - 1)
-      })
+      makeInstructionParser(
+        "mul",
+        (r: Char, v: BigInt) => (state: State) => state.mulInst(r, v))
+
     def add: Parser[Instruction] =
-      twoOperandInst("add", {
-        case (r, v) =>
-          (s: State) =>
-            s.write(r, v + s.read(r))
-      })
+      makeInstructionParser(
+        "add",
+        (r: Char, v: BigInt) => (state: State) => state.addInst(r, v))
+
     def mod: Parser[Instruction] =
-      twoOperandInst("mod", {
-        case (r, v) =>
-          (s: State) =>
-            s.write(r, s.read(r) % v)
-      })
-    def snd: Parser[Instruction] =
-      "snd" ~> register ^^ (r => (s: State) => s.send(s.read(r)))
-    def rcv: Parser[Instruction] =
-      "rcv" ~> register ^^ (r =>
+      makeInstructionParser(
+        "mod",
+        (r: Char, v: BigInt) => (state: State) => state.modInst(r, v))
+
+    def snd: Parser[Instruction] = "snd" ~> valueOrRegister ^^ {
+      case Left(r) =>
         (s: State) =>
-          if (s.read(r) != 0) {
-            val (value, state) = s.receive
-            state.write(r, value)
-          } else s)
+          s.sndInst(s.read(r))
+      case Right(i) =>
+        (s: State) =>
+          s.sndInst(i)
+    }
+
+    def rcv: Parser[Instruction] =
+      "rcv" ~> register ^^ (r => (s: State) => s.rcvInst(r))
+
+    def jgz: Parser[Instruction] =
+      "jgz" ~> valueOrRegister ~ valueOrRegister ^^ {
+        case Right(a) ~ Right(b) =>
+          (s: State) =>
+            s.jgzInst(a, b)
+        case Left(a) ~ Right(b) =>
+          (s: State) =>
+            s.jgzInst(s.read(a), b)
+        case Left(a) ~ Left(b) =>
+          (s: State) =>
+            s.jgzInst(s.read(a), s.read(b))
+        case Right(a) ~ Left(b) =>
+          (s: State) =>
+            s.jgzInst(a, s.read(b))
+      }
+
     def instruction =
-      set | mul | jmp | add | mod | snd | rcv
+      set | mul | jgz | add | mod | snd | rcv
 
     def analyse(input: String): Instruction = {
       parse(instruction, input) match {
@@ -137,22 +139,79 @@ object Main extends App {
     }
   }
 
-  def parser = (new InstructionParser)
+  @scala.annotation.tailrec
+  def runLoop(p: Prog, steps: Int = 10000): Prog =
+    if (steps == 0) p else runLoop(p.step, steps - 1)
+
+  var transmitted = 0
+
+  case class Prog(state: State,
+                  instructions: Seq[Instruction],
+                  pending: Boolean = false) {
+
+    def isBlocked: Boolean = pending && state.pipe.in.isEmpty
+    def setPipe(b: Pipe) = Prog(state.setPipe(b), instructions, pending)
+
+    def pipe(o: Prog): Tuple2[Prog, Prog] = {
+
+      transmitted += o.state.pipe.out.size
+      val ain = state.pipe.in.size
+      val bin = o.state.pipe.in.size
+      val apc = state.pc
+      val bpc = o.state.pc
+      println(
+        s"transmitted: $transmitted (a in: $ain pc: $apc, b in: $bin pc: $bpc)")
+
+      val (nextPipeA, nextPipeB) = state.pipe.connect(o.state.pipe)
+      (setPipe(nextPipeA), o.setPipe(nextPipeB))
+    }
+
+    def instString = input.toSeq(state.pc)
+    override def toString =
+      s"Instruction: $instString (pending $pending) $state"
+
+    def stepOnce: Prog =
+      try {
+        step
+      } catch {
+        case PendingInstruction(prog) =>
+          Prog(prog.state, prog.instructions, true)
+      }
+
+    def step: Prog =
+      try {
+        Prog(state.step(instructions).incrPC, instructions, false)
+      } catch {
+        case ReceiveInterrupt => throw new PendingInstruction(this)
+      }
+
+    def run: Prog =
+      try {
+        runLoop(this)
+      } catch {
+        case PendingInstruction(prog) =>
+          Prog(prog.state, prog.instructions, true)
+      }
+  }
+
   def input = Source.fromResource("day18.txt").getLines
   // def input = "set a 1; add a 2; mul a a; mod a 5; snd a; set a 0; rcv a; jgz a -1; set a 1; jgz a -2".split(";")
+  // def input = "snd 1; snd 2; snd p; rcv a; rcv b; rcv c; rcv d".split(";")
+
+  def parser = (new InstructionParser)
   def instructions: Seq[Instruction] = input.map(parser.analyse(_)).toSeq
 
-  val emptyBus = Bus(List(), List())
-  val a = State(Map(('p' -> 0)), 0, emptyBus)
-  val b = State(Map(('p' -> 1)), 0, emptyBus)
-
-  def loop(a: State, b: State): Tuple2[State,State] = {
-    val (nextA, nextB) = a.stepSafe(instructions).updateBus(b.stepSafe(instructions))
-    if (a.isEqual(nextA) && b.isEqual(nextB)) (a, b)
+  def loop(a: Prog, b: Prog): Tuple2[Prog, Prog] = {
+    val progs = List(a, b).par.map(_.run)
+    val (nextA, nextB) = progs.head.pipe(progs.tail.head)
+    if (nextA.isBlocked && nextB.isBlocked) (nextA, nextB)
     else loop(nextA, nextB)
   }
 
-  val (finalA, finalB) = loop(a, b)
-  println(finalA)
-  println(finalB)
+  def part2 {
+    val a = Prog(State(Map(('p' -> 0)), 0, Pipe()), instructions)
+    val b = Prog(State(Map(('p' -> 1)), 0, Pipe()), instructions)
+    val (finalA, finalB) = loop(a, b)
+  }
+  part2
 }
